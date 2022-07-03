0d416ab69c2202fbdf593da4074938a42417c196
Arash <97286379+Ghalbeyou@users.noreply.github.com>
2022-07-03 13:43:26 +0430

    Update Doxtool.bat

--- Doxtool.bat
+++ Doxtool.bat
@@ -1,23 +1,14 @@
 @echo off
-title We loading program, please wait
-echo Loading...
-ping localhost -n 3 >nul 
-echo Loading text color...
-ping localhost -n 3 >nul
-color 0c
-title Program ready to launch!
-echo Press any key to launch dox tool!
-pause
-GOTO :GUI
-:GUI
-title Dox tool by Decation
+title DoxTool
+echo LOADING ..
+ping localhost -n 5 > nul
 cls
-@echo off
-color 0c
-echo Welcome to the dox tool!
-SET /p doxtool="Type here the ip or nickname to dox:"
-start https://www.youtube.com/results?search_query=%doxtool%
-start https://ip-api.com/#%doxtool%
-pause
-GOTO :GUI
-
+:GUI
+set name=0
+echo DoxTool
+echo by: Decation2 and Ghalbeyou in github!
+set /p name=Name and last name of your victem: 
+if "%name%" equ 0 goto :GUI
+start https://www.whitepages.com/name/"%name%"
+cls
+goto :GUI
